22bdbcae4e09d7be57053c37fbc464baee4c08b8
kavya <kavyamounika.6@gmail.com>
2017-06-20 10:48:17 -0500

    Added dynamic port

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -2,13 +2,18 @@ package com.mulesoft.training;
 
 import static org.junit.Assert.*;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.mule.tck.junit4.FunctionalTestCase;
+import org.mule.tck.junit4.rule.DynamicPort;
 
 public class HelloMavenTest extends FunctionalTestCase {
 
-    @Test
+    @Rule
+    public DynamicPort dynamicPort = new DynamicPort("http.port");
+	@Test
     public void mavenFlowReturnsHelloMaven() throws Exception {
+		System.out.print("\n\nJunit Test case #1: Http Dynamic Port ----> "+dynamicPort.getNumber()+"\n\n");
         runFlowAndExpect("mavenFlow", "Hello Maven");
     }
     
